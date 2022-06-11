3734c4bbbefebee697889ba5200b60203d94aa59
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-06-11 18:27:26 -0500

    Creates GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Static instance of the Game Manager
+    //  can be accessed from anywhere
+    public static GameManager instance = null;
+
+    // Player score
+    public int score = 0;
+
+    // Called when the object is initialized
+    void Awake()
+    {
+        // if it doesn't exist
+        if(instance == null)
+        {
+            // Set the instance to the current object (this)
+            instance = this;
+        }
+
+        // There can only be a single instance of the Game Manager
+        else if(instance != this)
+        {
+            // Destroy the current object, so there is just one manager
+            Destroy(gameObject);
+        }
+
+        // Don't destroy this object when loading scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Increase score
+    public void IncreaseScore(int amount)
+    {
+        // Increase score by given amount
+        score += amount;
+
+        // Show the new score in the console
+        print("New Score: " + score.ToString());
+    }
+}
