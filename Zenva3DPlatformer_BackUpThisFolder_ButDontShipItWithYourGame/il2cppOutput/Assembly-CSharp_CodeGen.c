bfbff7c4eeb55c60a896d4a0d0689b3cefa16fa2
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-06-13 11:50:02 -0500

    Android version initial commit

---
+++ Zenva3DPlatformer_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,149 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CoinController::Update()
+extern void CoinController_Update_m92673DE20F8C5761A04BFBFB4B27E76090EF168D (void);
+// 0x00000002 System.Void CoinController::.ctor()
+extern void CoinController__ctor_mFEFFAC115A8889862E9D89A1D46FF820918177A4 (void);
+// 0x00000003 System.Void EnemyController::Start()
+extern void EnemyController_Start_mD47FA99710AFEDA91637E73B4109FC0125032604 (void);
+// 0x00000004 System.Void EnemyController::Update()
+extern void EnemyController_Update_mD4EAAD97042453D8F3F5D623882F24366451F7ED (void);
+// 0x00000005 System.Void EnemyController::.ctor()
+extern void EnemyController__ctor_m984FF7EBF9BF2923A09FA943862B8941E9FFECCD (void);
+// 0x00000006 System.Void GameManager::Awake()
+extern void GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F (void);
+// 0x00000007 System.Void GameManager::IncreaseScore(System.Int32)
+extern void GameManager_IncreaseScore_m2EC36BE6EA39E35997EB168AFB505F2D6CBABDE0 (void);
+// 0x00000008 System.Void GameManager::Reset()
+extern void GameManager_Reset_m7584A67E642737B6C76C139B05D75584D2975106 (void);
+// 0x00000009 System.Void GameManager::IncreaseLevel()
+extern void GameManager_IncreaseLevel_mAF8E6AF353B80F46AD5822A9EB7FC89C74EE7C1E (void);
+// 0x0000000A System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x0000000B System.Void GameOverUIManager::Start()
+extern void GameOverUIManager_Start_m92A157B1C7C90C0FEB6B36E6F7087C81C568D1C3 (void);
+// 0x0000000C System.Void GameOverUIManager::RestartGame()
+extern void GameOverUIManager_RestartGame_mEC2C6C600DFD08FDABC0444C3CF36312218B3210 (void);
+// 0x0000000D System.Void GameOverUIManager::.ctor()
+extern void GameOverUIManager__ctor_m26D6FC6F9FD2F51D690B38FD8F1D423CFBC9040E (void);
+// 0x0000000E System.Void HomeUIManager::StartGame()
+extern void HomeUIManager_StartGame_mC8849984439B55E28D325B8C5886F482C926D0EC (void);
+// 0x0000000F System.Void HomeUIManager::Start()
+extern void HomeUIManager_Start_m91C35E71686F91ACF323401C1C74B0D8B4F78DFE (void);
+// 0x00000010 System.Void HomeUIManager::Update()
+extern void HomeUIManager_Update_m301B776C88D4134B04E5930CC1F4F3714268E36D (void);
+// 0x00000011 System.Void HomeUIManager::.ctor()
+extern void HomeUIManager__ctor_m43413B5AE2327E0A2178D2ABAFE1EFFDC15CD7AF (void);
+// 0x00000012 System.Void HudManager::Start()
+extern void HudManager_Start_m8B735878412323DCF636D014649514407074D64F (void);
+// 0x00000013 System.Void HudManager::Refresh()
+extern void HudManager_Refresh_mB3D39DFDE12AEF2CA67E520E7DEB7D20CF920F9A (void);
+// 0x00000014 System.Void HudManager::.ctor()
+extern void HudManager__ctor_m936F54D1D49B5D41CA8407B06ABF871730D7DF74 (void);
+// 0x00000015 System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x00000016 System.Void PlayerController::Update()
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+// 0x00000017 System.Void PlayerController::WalkHandler()
+extern void PlayerController_WalkHandler_m7CA7A50B1DD69A91247DCFDDCD5A259000607DA5 (void);
+// 0x00000018 System.Void PlayerController::JumpHandler()
+extern void PlayerController_JumpHandler_m74281802BC9D5372E0EAE7A0DADDD74C9B45BCD5 (void);
+// 0x00000019 System.Boolean PlayerController::CheckGrounded()
+extern void PlayerController_CheckGrounded_mE977111C038A822DAEF246FB9A8C3F299279543E (void);
+// 0x0000001A System.Void PlayerController::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1 (void);
+// 0x0000001B System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+static Il2CppMethodPointer s_methodPointers[27] = 
+{
+	CoinController_Update_m92673DE20F8C5761A04BFBFB4B27E76090EF168D,
+	CoinController__ctor_mFEFFAC115A8889862E9D89A1D46FF820918177A4,
+	EnemyController_Start_mD47FA99710AFEDA91637E73B4109FC0125032604,
+	EnemyController_Update_mD4EAAD97042453D8F3F5D623882F24366451F7ED,
+	EnemyController__ctor_m984FF7EBF9BF2923A09FA943862B8941E9FFECCD,
+	GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F,
+	GameManager_IncreaseScore_m2EC36BE6EA39E35997EB168AFB505F2D6CBABDE0,
+	GameManager_Reset_m7584A67E642737B6C76C139B05D75584D2975106,
+	GameManager_IncreaseLevel_mAF8E6AF353B80F46AD5822A9EB7FC89C74EE7C1E,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	GameOverUIManager_Start_m92A157B1C7C90C0FEB6B36E6F7087C81C568D1C3,
+	GameOverUIManager_RestartGame_mEC2C6C600DFD08FDABC0444C3CF36312218B3210,
+	GameOverUIManager__ctor_m26D6FC6F9FD2F51D690B38FD8F1D423CFBC9040E,
+	HomeUIManager_StartGame_mC8849984439B55E28D325B8C5886F482C926D0EC,
+	HomeUIManager_Start_m91C35E71686F91ACF323401C1C74B0D8B4F78DFE,
+	HomeUIManager_Update_m301B776C88D4134B04E5930CC1F4F3714268E36D,
+	HomeUIManager__ctor_m43413B5AE2327E0A2178D2ABAFE1EFFDC15CD7AF,
+	HudManager_Start_m8B735878412323DCF636D014649514407074D64F,
+	HudManager_Refresh_mB3D39DFDE12AEF2CA67E520E7DEB7D20CF920F9A,
+	HudManager__ctor_m936F54D1D49B5D41CA8407B06ABF871730D7DF74,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController_WalkHandler_m7CA7A50B1DD69A91247DCFDDCD5A259000607DA5,
+	PlayerController_JumpHandler_m74281802BC9D5372E0EAE7A0DADDD74C9B45BCD5,
+	PlayerController_CheckGrounded_mE977111C038A822DAEF246FB9A8C3F299279543E,
+	PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+};
+static const int32_t s_InvokerIndices[27] = 
+{
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	2792,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3448,
+	3296,
+	2810,
+	3448,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	27,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
